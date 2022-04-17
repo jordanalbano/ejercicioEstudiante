aec1403a59163795727973d66175c6c8a6affac3
jordanalbano <jordanalbano86@gmail.com>
2022-04-17 20:12:46 -0300

    tareas modulo 2 terminadas

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import Persona from "./Persona"
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+  <Persona/>
   </React.StrictMode>,
   document.getElementById('root')
 );
