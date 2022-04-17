f927ece5f335ec2c78dc8f412b67de93e97672c3
jordanalbano <jordanalbano86@gmail.com>
2022-04-17 19:00:41 -0300

    cambio 2

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,7 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
+  //push a ver si sale
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
